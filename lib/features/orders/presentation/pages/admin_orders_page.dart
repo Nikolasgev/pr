865c137e190c12798faeec0d91d1a8088c2d5f3e
Nikolas <nikolasgev@gmail.com>
2025-03-26 08:42:39 +0300

    fix

--- lib/features/orders/presentation/pages/admin_orders_page.dart
+++ lib/features/orders/presentation/pages/admin_orders_page.dart
@@ -15,7 +15,7 @@ class AdminOrdersPage extends StatelessWidget {
       child: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            title: const Text('Заказы 012'),
+            title: const Text('Заказы 013'),
           ),
           body: BlocBuilder<AdminOrdersBloc, AdminOrdersState>(
             builder: (context, state) {
